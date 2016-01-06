6443275fc6f791b138ec251f58a2ebc2cebcd335
unyon <michalloran@hotmail.com>
2016-01-06 22:46:12 +0000

    brand navbar fot

--- config/application.rb
+++ config/application.rb
@@ -20,7 +20,6 @@ module Nomster
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
   
-
     # Add the fonts path
     config.assets.paths << "#{Rails.root}/app/assets/fonts"
 
