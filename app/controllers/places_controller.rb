488c75f54316181ab613127df7dede44e32d803a
unyon <michalloran@hotmail.com>
2016-01-07 16:12:13 +0000

    heroku assets

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,7 +1,7 @@
 class PlacesController < ApplicationController
 
 def index
-	@places = Place.all.paginate(:page => params[:page], :per_page => 2)
+	@places = Place.all.paginate(:page => params[:page], :per_page => 10)
 end
 
 end
