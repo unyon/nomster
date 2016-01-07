a8ab690e38f368d32236e27afb3c400bc790f121
unyon <michalloran@hotmail.com>
2016-01-07 18:20:21 +0000

    map form add created places to db

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -9,4 +9,16 @@ def new
 	@places = Place.new
 end
 
+def create
+	Place.create(place_params)
+	redirect_to root_path
+end
+
+private
+
+def place_params
+	params.require(:place).permit(:name, :address, :description)
+end
+
+
 end
