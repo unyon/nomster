b4198ca6f679b270f5a45ccaca2cf3bbce9c9f60
unyon <michalloran@hotmail.com>
2016-01-08 17:17:26 +0000

    restrict edit access to user who created place

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,7 @@
 class PlacesController < ApplicationController
-	before_action :authenticate_user!, :only => [:new, :create]
+	before_action :authenticate_user!, :only => [:new, :create, :edit, :update, :destroy]
+
+
 
 def index
 	@places = Place.all.paginate(:page => params[:page], :per_page => 10).order('created_at ASC')
@@ -7,11 +9,11 @@ end
 
 
 def new
-	@places = Place.new
+	@places = Place.news
 end
 
 def create
-	current_users.places.create(place_params)
+	current_user.places.create(place_params)
 	redirect_to root_path
 end
 
@@ -21,9 +23,18 @@ end
 
 def edit
 	@place = Place.find(params[:id])
+
+	if @place.user != current_user 
+		return render :text => 'Not allowed', :status => :forbidden
+	end
 end
 
 def update
+
+	if @place.user != current_user 
+		return render :text => 'Not allowed', :status => :forbidden
+	end
+	
 	@place = Place.find(params[:id])
 	@place.update_attributes(place_params)
 	redirect_to root_path
