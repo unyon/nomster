ccf64f75480f145b3563072ce92405609fd1d30c
unyon <michalloran@hotmail.com>
2016-01-12 19:03:39 +0000

    send automated emails when there is a new email

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,10 +1,13 @@
 class NotificationMailer < ActionMailer::Base
     default from: "no-reply@nomsterapp.com"
 
-def comment_added
-	mail(to: "michalloran@hotmail.com",
-		subject: "A comment has been added to your place.")
-end
+  def comment_added(comment)
+    @place = comment.place
+    @place_owner = @place.user
+    mail(to: @place_owner.email,
+         subject: "A comment has been added to #{@place.name}")
+  end
+
 
 
 end
