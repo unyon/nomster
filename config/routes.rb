2f534090b43e2238a45a7a181b12e16093703b37
unyon <michalloran@hotmail.com>
2016-01-14 16:32:09 +0000

    add user show page and link to my profile

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Nomster::Application.routes.draw do
   devise_for :users
   root 'places#index'
-
+  
   resources :places do
-    resources :comments, :only => :create
-    resources :photos, :only => :create
+    resources :comments, only: :create
+    #resources :users, only: :show 
   end
-  resources :users, :only => :show
+
+resources :users, :only => [:show]
+
 
 
   # The priority is based upon order of creation: first created -> highest priority.
