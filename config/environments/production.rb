c8127ff7b31ef22fa429be12e22d91728eb75f82
unyon <michalloran@hotmail.com>
2016-01-15 19:52:14 +0000

    fix profile page error

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,10 +80,6 @@ Nomster::Application.configure do
   # the I18n.default_locale when a translation can not be found).
   config.i18n.fallbacks = true
 
-  require 'jquery/modal/rails'
-  require 'jquery/modal/filters'
-  require 'jquery/modal/helpers'
-
 
 
   # Send deprecation notices to registered listeners.
