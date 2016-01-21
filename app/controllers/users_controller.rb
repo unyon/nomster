dd8ac7fcb23f7101c1852ba07bcbba48da14f427
unyon <michalloran@hotmail.com>
2016-01-21 16:29:11 +0000

    add user show page test

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,10 @@
 class UsersController < ApplicationController
 
-def show
-	@user = User.find(params[:id])
-end
+	def show
+			@user = User.where(id: params[:id]).first
+		if @user.blank?
+			render text: "Not found", status: :not_found
+	    end
+	end
 
 end
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,7 +1,19 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "create" do
+    user = FactoryGirl.create(:user)
+    sign_in user
+
+    get :show, id: user.id
+    assert_response :success
+
+   end
+
+  test "user show page, not found" do
+    get :show, id: 'OMG'
+    assert_response :not_found
+  end
+
+
 end
